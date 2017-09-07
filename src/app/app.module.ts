585fb77c352b49d1c3c213c1d490bce8cb061e17
Simão Menezes <simaomenezesneto@gmail.com>
2017-09-07 10:39:00 -0300

    Criando componente para detalhes de restaurants, shopping-car, restaurant-item. Criando novas rotas para os componentes etc.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,9 @@
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { RestaurantsService } from './restaurants/restaurants.service';
+
 import { ROUTES } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -13,7 +18,6 @@ import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +25,11 @@ import { RestaurantComponent } from './restaurants/restaurant/restaurant.compone
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
