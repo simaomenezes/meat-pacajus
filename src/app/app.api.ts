62e0ecb5e12fc0e76864d0e094a315056397efd0
Simão Menezes <simaomenezesneto@gmail.com>
2017-09-07 10:08:27 -0300

    Refatorando o servico de restaurants, criando uma constant para nossa api de dados.

---
+++ src/app/app.api.ts
@@ -0,0 +1,1 @@
+export const MEAT_PACAJUS_API = 'http://localhost:3000'
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,32 +1,18 @@
+import { Injectable } from '@angular/core'
+import { Http } from '@angular/http'
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/map'
 import { Restaurant } from './restaurant/restaurant.model';
 
+import { MEAT_PACAJUS_API } from './../app.api';
+
+@Injectable()
+
 export class RestaurantsService {
-    constructor(){}
 
-    restaurantsRest: Restaurant[] = [
-        {
-          id: "bread-bakery",
-          name: "Bread & Bakery",
-          category: "Bakery",
-          deliveryEstimate: "25m",
-          rating: 4.9,
-          imagePath: "assets/img/restaurants/breadbakery.png"
-          //about: "A Bread & Brakery tem 40 anos de mercado. Fazemos os melhores doces e pães. Compre e confira.",
-          //hours: "Funciona de segunda à sexta, de 8h às 23h"
-        },
-        {
-          id: "burger-house",
-          name: "Burger House",
-          category: "Hamburgers",
-          deliveryEstimate: "100m",
-          rating: 3.5,
-          imagePath: "assets/img/restaurants/burgerhouse.png"
-          //about: "40 anos se especializando em trash food.",
-         //hours: "Funciona todos os dias, de 10h às 22h"
-        }
-      ]
+    constructor(private http: Http){}
 
-    restaurants(): Restaurant[] {
-        return this.restaurantsRest;
+    restaurants(): Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_PACAJUS_API}/restaurants`).map(res => res.json())
     }
 }
