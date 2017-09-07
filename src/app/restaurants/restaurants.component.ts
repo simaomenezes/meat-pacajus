0ffd06f763d0f5b575745be25bd86c9a317739bd
Simão Menezes <simaomenezesneto@gmail.com>
2017-09-07 09:45:55 -0300

    Criando servico para restaurantes, chamando servico no componente, configurando servico no app-modules

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,3 +1,4 @@
+import { RestaurantsService } from './restaurants.service';
 import { Restaurant } from './restaurant/restaurant.model';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,32 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantsComponent implements OnInit {
 
-  restaurants: Restaurant[] = [
-    {
-      id: "bread-bakery",
-      name: "Bread & Bakery",
-      category: "Bakery",
-      deliveryEstimate: "25m",
-      rating: 4.9,
-      imagePath: "assets/img/restaurants/breadbakery.png"
-      //about: "A Bread & Brakery tem 40 anos de mercado. Fazemos os melhores doces e pães. Compre e confira.",
-      //hours: "Funciona de segunda à sexta, de 8h às 23h"
-    },
-    {
-      id: "burger-house",
-      name: "Burger House",
-      category: "Hamburgers",
-      deliveryEstimate: "100m",
-      rating: 3.5,
-      imagePath: "assets/img/restaurants/burgerhouse.png"
-      //about: "40 anos se especializando em trash food.",
-     //hours: "Funciona todos os dias, de 10h às 22h"
-    }
-  ]
+  restaurants: Restaurant[];
 
-  constructor() { }
+  constructor(private restaurantService: RestaurantsService) { }
 
   ngOnInit() {
+    this.restaurants = this.restaurantService.restaurants();
   }
 
 }
