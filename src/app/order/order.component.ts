58cfa4de1b0808dda6fe9f8550ff55b438f21d52
Simão Menezes <simaomenezesneto@gmail.com>
2017-12-23 18:22:23 -0300

    Finalizando compras com Http POST

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -2,6 +2,7 @@ import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model';
 import { OrderService } from './order.service';
 import { RadioOption } from './../shared/radio/radio-options.model';
 import { Component, OnInit } from '@angular/core';
+import { Order, OrderItem } from 'app/order/order';
 
 @Component({
   selector: 'mt-order',
@@ -40,6 +41,17 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem){
     this.orderService.remove(item);
+  }
+
+  checkOrder(order: Order){
+    order.orderItems = this.carItems().map((item:CartItem)=> new OrderItem(item.quantity, item.menuItem.id));
+    this.orderService.checkOrder(order).subscribe(
+        (orderId: string) => {
+          console.log(`Compra concluída: ${orderId}`);
+          this.orderService.clear();
+        }
+    );
+    console.log(order);
   }
 
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,17 @@
+import { MEAT_PACAJUS_API } from './../app.api';
+import { Http, Headers, ResponseOptions, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { Order } from 'app/order/order';
 import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model';
 import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shopping-cart.service';
 import { Injectable } from '@angular/core';
 
+
 @Injectable()
 export class OrderService {
 
-  constructor(private carService: ShoppingCartService) { }
+  constructor(private carService: ShoppingCartService, private http: Http) { }
 
   carItems(): CartItem[]{
     return this.carService.items;
@@ -25,6 +31,22 @@ export class OrderService {
 
   itemsValue(): number {
     return this.carService.total();
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.post(
+        `${MEAT_PACAJUS_API}/orders`, 
+        JSON.stringify(order), 
+        new RequestOptions({headers: headers})).map(res => res.json()
+      );
+  }
+
+
+  clear(){
+    this.carService.clear();
   }
 
 }
